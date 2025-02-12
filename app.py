59069d36c1804a55d3d4c403a1aa9af96a18c976
mokkieeee <darrenmok127@gmail.com>
2025-02-12 08:19:18 +0000

    1

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import render_template,request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET","POST"])
 def index():
     return(render_template("index.html"))
 
