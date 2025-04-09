4b05f07015334149e7f24147462328f80127f56c
mokkieeee <darrenmok127@gmail.com>
2025-04-09 07:15:11 +0000

    1#

--- app.py
+++ app.py
@@ -15,17 +15,17 @@ def prediction():
 @app.route("/predicted_DBS", methods=["GET","POST"])
 def predicted_DBS():
     q = float(request.form.get("q"))
-    return(render_template("predicted_DBS.html", r=(-50.6*q)+90.2))
+    return(render_template("predicted_DBS.html",r=(-50.6*q)+90.2))
 
 @app.route("/text_sentiment", methods=["GET","POST"])
 def text_sentiment():
-    return(render_template("text_sentiment.html")) 
+    return(render_template("text_sentiment.html"))
 
 @app.route("/sentiment_result", methods=["GET","POST"])
 def sentiment_result():
-    q =request.form.get("q")
+    q = request.form.get("q")
     r = textblob.TextBlob(q).sentiment
-    return(render_template("sentiment_result.html", r=r))
+    return(render_template("sentiment_result.html",r=r))
 
 @app.route("/paynow", methods=["GET","POST"])
 def paynow():
@@ -33,4 +33,3 @@ def paynow():
 
 if __name__ == "__main__":
     app.run()
-    
